d9b5ee2c2afb3f5d4b3eb72391cf34f23091c28b
cpeterdy <cpeterdy@gmail.com>
2026-08-02 19:20:34 -0400

    added basic JS and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Recipe will appear here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
