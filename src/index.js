c7c71d5c2ab7fa070c3f4f31cfb5ca470259ec0d
cpeterdy <cpeterdy@gmail.com>
2026-08-02 21:13:40 -0400

    added project finishing touches

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateRecipe(event) {
   let apiKey = "3oe1c54adcb9b00207cc24fbt2d43a1d";
   let prompt = `User instructions are to generate a recipe using ${instructionsInput.value}`;
   let context =
-    "You are a food expert and chef. Your mission is to generate a delicious recipe for people cooking at home in basic HTML. The recipe should be easy to follow. Include the ingredients, steps to follow and cook time. Make sure to follow the user instructions submitted. Do not tell me that the recipe is in HTML. ";
+    "You are a food expert and chef. Your mission is to generate a delicious recipe for people cooking at home in basic HTML. The recipe should be simple and easy to follow. Include the ingredients, steps to follow and cook time. Make sure to follow the user instructions submitted. Do not tell me that the recipe is in HTML. ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeElement = document.querySelector("#recipe");
